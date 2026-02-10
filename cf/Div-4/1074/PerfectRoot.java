f908bc2e6a0a3a2546933b1d5c170e8289a97af5
krishsharma955 <krishsharma9005@gmail.com>
2026-02-10 23:04:45 +0530

    PerfectRoot & template

--- cf/Div-4/1074/PerfectRoot.java
+++ cf/Div-4/1074/PerfectRoot.java
@@ -1,13 +1,90 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class PerfectRoot {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t-- > 0) {
-            int n = sc.nextInt();
-            
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
         }
-        sc.close();
+        
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+        
+        String nextLine() {
+            String str = "";
+            try {
+                if (st != null && st.hasMoreTokens()) {
+                    str = st.nextToken("\n");
+                } else {
+                    str = br.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+        
+        int[] readIntArray(int n) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+        
+        long[] readLongArray(int n) {
+            long[] arr = new long[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextLong();
+            }
+            return arr;
+        }
+    }
+    
+    // Main solution method
+    static void solve(FastReader sc) {
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+    }
+    
+    public static void main(String[] args) {
+        FastReader sc = new FastReader();
+        
+        // For multiple test cases, uncomment:
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            solve(sc);
+        }
+
     }
 }
